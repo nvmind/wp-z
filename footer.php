bb0213ec2b9d0deae23851be97d131c0efe86b0b
Ignacio Chavez <nashio@Ignacios-MacBook-Air.local>
2013-03-01 23:48:12 -0800

    first commit

---
+++ footer.php
@@ -0,0 +1,70 @@
+			<footer class="footer" role="contentinfo">
+			
+				<div id="inner-footer" class="wrap clearfix">
+					
+					<nav role="navigation">
+    					<?php bones_footer_links(); ?>
+	                </nav>
+	                		
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+				
+				</div> <!-- end #inner-footer -->
+				
+			</footer> <!-- end footer -->
+		
+		</div> <!-- end #container -->
+		
+		<!-- all js scripts are loaded in library/bones.php -->
+		<?php wp_footer(); ?>
+
+
+	
+
+		<script>
+
+		document.write('<script src=' +
+		('__proto__' in {} ? '<?php bloginfo('template_url');?>/library/js/libs/zepto' : '<?php bloginfo('template_url');?>/library/js/vendor/jquery') +
+		'.js><\/script>')
+		</script>
+
+	
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.js"></script>
+		
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.dropdown.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.alerts.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.clearing.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.placeholder.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.forms.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.cookie.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.joyride.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.magellan.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.orbit.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.reveal.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.tooltips.js"></script>
+		
+		<script src="<?php bloginfo('template_url');?>/library/js/libs/foundation/foundation.topbar.js"></script>
+		
+		
+		
+		<script>
+		  $(document).foundation();
+		</script>
+
+
+	</body>
+
+</html> <!-- end page. what a ride! -->
